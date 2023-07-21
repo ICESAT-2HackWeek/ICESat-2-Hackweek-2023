92ff5652533ff243b1260c10759ea313f73e6938
Joachim Meyer <jomey@uw.edu>
2023-07-21 10:39:44 -0600

    Conda - Update lock-environment.sh script
    Bump used version of `conda-lock` and `mamba` to the current released
    version. Also add better error handling during the script execution.

--- conda/lock-environment.sh
+++ conda/lock-environment.sh
@@ -5,11 +5,21 @@
 ENV_FILE="environment.yml"
 LOCK_ENV='CondaLock'
 
+check_success() {
+  if [[ $1 -ne 0 ]]; then
+    printf "\033[1;31m ERROR \033[0m\n"
+    exit 1
+  else
+    printf "\033[1;32m SUCCESS \033[0m\n"
+  fi
+  printf $2
+}
+
 # Generate CondaLock environment unless present
 conda env list | grep ${LOCK_ENV} > /dev/null
 
 if [[ $? -eq 1 ]]; then
-  conda create -q -y -n ${LOCK_ENV} -c conda-forge conda-lock=1.0.3 mamba=0.22
+  conda create -q -y -n ${LOCK_ENV} -c conda-forge conda-lock=2.1.1 mamba=1.4.9
 fi
 
 # https://github.com/conda/conda/issues/7980#issuecomment-492784093
@@ -25,20 +35,17 @@ fi
 # Local environments
 ## Generate explicit lock files
 conda-lock lock --mamba -f ${ENV_FILE}
+lock_success=$?
+check_success lock_success "  Building local environments"
 
 # BinderHub support
 ## Generate environment.yml for binder compatibility
-printf "Generate environment.yml for BinderHub \n"
 conda-lock render -k env
-
-# Temporary fix for https://github.com/conda-incubator/conda-lock/issues/172
-sed -i.bak -r 's/--hash=md5:None//' conda-linux-64.lock.yml
-sed -i.bak -r 's/--hash=md5:None//' conda-osx-64.lock.yml
-rm *.yml.bak
-
+lock_success=$?
+check_success lock_success "Generate environment.yml for BinderHub \n"
 
 # Remove CondaLock environment when the last command was successful
-if [[ $? -eq 0 ]]; then
+if [[ lock_success -eq 0 ]]; then
   conda deactivate
   conda remove -q -y --name ${LOCK_ENV} --all
 fi
